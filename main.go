1c24c5b609561c223d6e19f4ee476696aeb5aa55
Joshua Johnson <joshuaj1397@gmail.com>
2018-03-12 20:48:28 -0500

    Added Summoner model for getting a summoner from the RIOT Api

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
 func index(w http.ResponseWriter, req *http.Request) {
 	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
 	if err != nil {
@@ -32,15 +36,23 @@ func memed(w http.ResponseWriter, req *http.Request) {
 		fd.SummonerName = req.FormValue("summonerName")
 		fd.ChampionName = req.FormValue("championName")
 	}
+	// s, err := GetSummoner(fd.SummonerName)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	// 	return
+	// }
 
 	err := tpl.ExecuteTemplate(w, "memed.gohtml", fd)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 }
 
 func main() {
+	log.Println("Freak Face 3.")
 	http.HandleFunc("/", index)
 	http.HandleFunc("/memed", memed)
 	http.ListenAndServe(":8080", nil)
